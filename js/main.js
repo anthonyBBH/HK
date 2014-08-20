12cd1969c90bf49a09fbefd1200ecb6f411e58a3
Anthony Terruso <anthony.terruso@bartleboglehegarty.com>
2014-08-20 11:56:03 -0400

    third module in and responsive

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
+if($(window).width() > 768){
 
-jQuery(document).ready(function() {
-    jQuery('.tabs .tab-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
- 
-        // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide(); 
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
- 
-        e.preventDefault();
-    });
+// Hide all but first tab content on larger viewports
+$('.accordion__content:not(:first)').hide();
+
+// Activate first tab
+$('.accordion__title:first-child').addClass('active');
+
+} else {
+  
+// Hide all content items on narrow viewports
+$('.accordion__content').hide();
+};
+
+// Wrap a div around content to create a scrolling container which we're going to use on narrow viewports
+$( ".accordion__content" ).wrapInner( "<div class='overflow-scrolling'></div>" );
+
+// The clicking action
+$('.accordion__title').on('click', function() {
+$('.accordion__content').hide();
+$(this).next().show().prev().addClass('active').siblings().removeClass('active');
 });
+
